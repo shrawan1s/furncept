794cc76034cb0fe97e8979199936f39c91380855
Shrawan <shrawanthakur0962@gmail.com>
2024-08-19 05:00:38 +0530

    Removed some files from redux setup

--- frontend/src/app/store.ts
+++ frontend/src/app/store.ts
@@ -1,13 +1,9 @@
 import { configureStore } from '@reduxjs/toolkit';
 import authReducer from './slices/authSlice';
-import customerReducer from './slices/customerSlice';
-import packingDataReducer from './slices/packingDataSlice';
 
 const store = configureStore({
   reducer: {
     auth: authReducer,
-    customer: customerReducer,
-    packingData: packingDataReducer,
   },
 });
 
